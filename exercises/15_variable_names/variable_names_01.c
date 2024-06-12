123b863da8098c6ac494b0f43010540b2b6de15e
danwritecode <dnelson4993@outlook.com>
2024-06-11 21:20:09 -0400

    added exercise

---
+++ exercises/15_variable_names/variable_names_01.c
@@ -0,0 +1,21 @@
+/*
+ * Variable naming seems pretty simple however there are some gotchas. 
+ *
+ * The primary gotchas are: "reserved words" and "illegal characters".
+ *
+ * Take a look at the below exercise and identify the variables that are 
+ * causing the compilation error.
+*/
+
+// ❌ I AM NOT DONE
+
+int main() {
+  // [TODO] Identify the variable(s) that is causing the compilation error. 
+  char foo1 = 'a';
+  float float = 1.0;
+  int bar = 1;
+  char 1baz = 'a';
+  
+	return 0;
+}
+
