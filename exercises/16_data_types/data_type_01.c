1b59d1647c92a6a82557a901fcf197f9fab555b9
Petr Spac <petrik.spac@gmail.com>
2024-06-15 20:44:28 +0200

    feat: data types

---
+++ exercises/16_data_types/data_type_01.c
@@ -0,0 +1,46 @@
+/*
+ * In this exercise, you will practice understanding data types and sizes in C.
+ * The basic data types in C include `char`, `int`, `float`, and `double`.
+ * You can also apply qualifiers like `short`, `long`, `signed`, and `unsigned` to these types,
+ * which affect their size and behavior. Understanding how these types and qualifiers
+ * work at a binary level is crucial for robust C programming.
+ *
+ * The provided file will not compile or run correctly until
+ * the user fixes the mistakes.
+ * (You can only delete/edit lines with // <-- Fix this line)
+*/
+
+// ❌ I AM NOT DONE
+
+#include <assert.h>
+#include <stdio.h>
+#include <limits.h>
+#include <stdbool.h>
+
+ // Note: Carefully observe the output to understand the underlying behavior caused by type limits and conversions.
+ // |char| <= |short| <= |int| <= |long| <= |long long|
+
+int main() {
+    unsigned int negativeAsUnsigned = -5; // <-- Fix this kind of misuse
+
+    long long veryLargeNumber = 9223372036854775807;
+    printf("Maximum long long value: %l\n", veryLargeNumber); // <-- Fix this line
+
+    printf("Range of unsigned long long: 0 to %llu\n", ); // <-- Fix this line
+    printf("Range of signed long long: %lld to %lld\n", ); // <-- Fix this line
+
+    double floatingPointNumber = 0.999999;
+    int integerRepresentation = floatingPointButNotReally; // <-- Fix this line (use floatingPointNumber)
+    printf("Double to int casting result: %d\n", integerRepresentation);
+
+	bool understand = true;
+	unsigned int plus_one = 1;
+	int minus_one = -1;
+
+	if(plus_one < minus_one) { // <-- Fix this line
+		understand = false;
+	} 
+
+	assert(understand && "1 < -1");
+	return 0;
+}
